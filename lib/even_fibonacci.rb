a4fc673de56b8938dd52ad67f82f1a1ac5a07061
hwalborn <hwalborn@gmail.com>
2017-05-15 12:36:20 -0400

    The holy grail

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,17 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  arr = [1, 2]
+  num = 0
+  i = 0
+
+  loop do
+    num = arr[i] + arr[i + 1]
+    break if num > limit
+    arr.push(num)
+    i += 1
+  end
+
+  arr.reduce(0) do |total, num|
+    num % 2 == 0 ? total += num : total
+  end
+end
