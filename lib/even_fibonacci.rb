1490dba15d4366829785609f77f85561751b9b6d
fakebenjay <fakebenjay@gmail.com>
2017-05-15 00:05:24 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,21 @@
-# Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  fib_seq = [1, 2]
+  counter = 1
+
+  while fib_seq[counter] < limit
+    counter += 1
+    fib_seq[counter] = fib_seq[counter - 1] + fib_seq[counter - 2]
+  end
+
+  fib_seq.pop
+
+  even_fib = fib_seq.map do |i|
+    if i % 2 == 0
+      i
+    else
+      i = 0
+    end
+  end
+
+  even_fib.inject { |sum, i| sum + i }
+end
